a030ec6bf5b885221fc00c1ffbed930a732f8aac
mohammadreza kari <mohammadreza.k@780.ir>
2023-04-09 15:50:30 +0330

    add wallet table schema

--- domain/wallet.go
+++ domain/wallet.go
@@ -1,1 +1,24 @@
 package domain
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Wallet struct {
+	ID        uuid.UUID
+	UserId    uuid.UUID
+	FirstName string
+	LastName  string
+	Amount    uint64
+	Status    Status
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Status string
+
+const (
+	Disabled Status = "disabled"
+	Enabled  Status = "enabled"
+)
